c46965ad63285f215ddbec00d4946131f60e971c
sofiazgureanu <57046690+sofiazgureanu@users.noreply.github.com>
2020-01-25 16:27:19 -0500

    ah yes

--- ElevatorRobot/src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ ElevatorRobot/src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -5,42 +5,44 @@
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
 
-package frc.robot.subsystems;
+package frc.robot.subsystems                                                    ;
 
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import edu.wpi.first.wpilibj.SpeedController;
-import edu.wpi.first.wpilibj.SpeedControllerGroup;
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import com.ctre.phoenix.motorcontrol.can.*;
-import com.ctre.phoenix.sensors.PigeonIMU;
+import edu.wpi.first.wpilibj2.command.SubsystemBase                             ;
+import edu.wpi.first.wpilibj.SpeedController                                    ;
+import edu.wpi.first.wpilibj.SpeedControllerGroup                               ;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive                            ;
+import com.ctre.phoenix.motorcontrol.can.*                                      ;
+
 
 
 
 /**
  * Add your docs here.
  */
-public class DriveSubsystem extends SubsystemBase {
+public class DriveSubsystem extends SubsystemBase                               {
 
-  DifferentialDrive m_drive;
+  DifferentialDrive m_drive                                                     ;
   // SpeedController m_falcon;
   // Put methods for controlling this subsystem
   // here. Call these from Commands.
-  public DriveSubsystem(){
+  public DriveSubsystem()                                                       {
     SpeedController m_frontLeft = new WPI_TalonSRX(6); //has the pigeon connected in AutoSubsystem
-    SpeedController m_rearLeft = new WPI_TalonSRX(1);
-    SpeedControllerGroup m_left = new SpeedControllerGroup(m_frontLeft, m_rearLeft);
+    SpeedController m_rearLeft = new WPI_TalonSRX(1)                            ;
+    SpeedControllerGroup m_left = new SpeedControllerGroup(m_frontLeft, m_rearLeft)
+                                                                                ;
  
-    SpeedController m_frontRight = new WPI_TalonSRX(4);
-    SpeedController m_rearRight = new WPI_TalonSRX(7);
-    SpeedControllerGroup m_right = new SpeedControllerGroup(m_frontRight, m_rearRight);
+    SpeedController m_frontRight = new WPI_TalonSRX(4)                          ;
+    SpeedController m_rearRight = new WPI_TalonSRX(7)                           ;
+    SpeedControllerGroup m_right = new SpeedControllerGroup(m_frontRight, m_rearRight)
+                                                                                ;
     // m_falcon = new WPI_TalonFX(0);
  
-    m_drive = new DifferentialDrive(m_left, m_right);
-    }
+    m_drive = new DifferentialDrive(m_left, m_right)                            ;
+                                                                                }
     
-    public void tankDrive(double leftSpeed, double rightSpeed){
-      m_drive.tankDrive(leftSpeed, rightSpeed);
-    }
+    public void tankDrive(double leftSpeed, double rightSpeed)                  {
+      m_drive.tankDrive(leftSpeed, rightSpeed)                                  ;
+                                                                                }
 
   
   
